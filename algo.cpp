fa1468927aa7605a168a7b97518e9ca7dd828f7f
Digvijay Chavan <chavandigvijay9081@gmail.com>
2023-09-05 22:40:28 +0530

    sortCompa

--- algo.cpp
+++ algo.cpp
@@ -3,11 +3,11 @@
 #include<vector>
 using namespace std;
 bool comp(int a,int b){
-    return a>b;
+    return a<b;
 }
 int main(){
     vector<int> vec{1,3,6,3,89,0,6,7};
-    sort(vec.begin(),vec.end());
+    sort(vec.begin(),vec.end(),comp);
     for(auto it: vec){
         cout<<it<<endl;
     }
